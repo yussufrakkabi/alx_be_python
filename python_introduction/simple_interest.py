e132281a0a741e8697bd68634b5bdeab1b567e8f
yussufrakkabi <yussufrakkabi@outlook.com>
2024-09-15 10:58:49 -0700

    commit simple interest py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+principal = 1000  #representing $1000
+rate = 0.05       #representing 5% annual interest rate)
+time = 3          #representing 3 years)
+
+                #(I = P * R * T)
+interest = principal * rate * time
+
+print(f"The simple interest is{interest}")
