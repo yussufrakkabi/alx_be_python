fb69feaca3e5569c44d4547f64c475849076c772
yussufrakkabi <yussufrakkabi@outlook.com>
2024-09-15 10:19:48 -0700

    add python introduction folder ...

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+sum_value = number1 + number2
+difference_value = number1 - number2
+product_value = number1 * number2
+
+print(f"Sum of {number1} and {number2} is {sum_value}.")
+print(f"Difference of {number1} and {number2} is {difference_value}.")
+print(f"Product of {number1} and {number2} is {product_value}.")
+
