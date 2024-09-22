9003e9ad3100bd5e1e1f2778c020728b92329613
yussufrakkabi <yussufrakkabi@outlook.com>
2024-09-22 03:27:13 -0700

    multiplication table

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,8 @@
+# multiplication_table.py
+#Multiplication Table Generator:
+number = int(input("Enter a number to see its multiplication table: "))
+
+for i in range(1, 11):
+    product = number * i
+    print(f"{number} * {i} = {product}")
+
