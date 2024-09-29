1ccbd39dbd703cb2b77d0f6554110d234dc33b5f
yussufrakkabi <yussufrakkabi@outlook.com>
2024-09-29 14:24:50 -0700

    add fns and dsa ...

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,18 @@
+#arithmetic_operations.py
+#0. Arithmetic Operations Function
+
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Cannot divide by zero"
+        else:
+            return num1 / num2
+    else:
+        return "Invalid operation"
+    
